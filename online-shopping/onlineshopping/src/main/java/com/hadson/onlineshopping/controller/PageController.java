a1d177dc472dc8204af16cfc288e533104c66496
Pavel <rogozhin.p@yandex.ru>
2020-02-08 00:22:40 +0300

    work with parametrs in url

--- online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/controller/PageController.java
+++ online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/controller/PageController.java
@@ -2,6 +2,7 @@ package com.hadson.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +15,19 @@ public class PageController {
 	mv.addObject("greeting", "Welcome to Spring Web MVC");
 	return mv;
 	
+    }
+
+    @RequestMapping(value="/test")
+    public ModelAndView test(@RequestParam
+			     (value="greeting", required=false)
+			     String greeting){
+	if(greeting == null) {
+	    greeting = "Hello there";
+	}
+
+	ModelAndView mv = new ModelAndView("page");
+	mv.addObject("greeting", greeting);
+	return mv;
     }
 	
 }
