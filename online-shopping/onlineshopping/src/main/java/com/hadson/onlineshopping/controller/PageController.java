aff2e97eb2c43dae7f7d3ca6998a5d43e36113f0
Pavel <rogozhin.p@yandex.ru>
2020-02-08 00:35:20 +0300

    make url frendly for seo

--- online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/controller/PageController.java
+++ online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.hadson.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,10 +18,22 @@ public class PageController {
 	
     }
 
-    @RequestMapping(value="/test")
-    public ModelAndView test(@RequestParam
-			     (value="greeting", required=false)
-			     String greeting){
+//    @RequestMapping(value="/test")
+//    public ModelAndView test(@RequestParam
+//			     (value="greeting", required=false)
+//			     String greeting){
+//	if(greeting == null) {
+//	    greeting = "Hello there";
+//	}
+//
+//	ModelAndView mv = new ModelAndView("page");
+//	mv.addObject("greeting", greeting);
+//	return mv;
+//    }
+    
+    @RequestMapping(value="/test/{greeting}")
+    public ModelAndView test(
+			     @PathVariable("greeting")String greeting){
 	if(greeting == null) {
 	    greeting = "Hello there";
 	}
