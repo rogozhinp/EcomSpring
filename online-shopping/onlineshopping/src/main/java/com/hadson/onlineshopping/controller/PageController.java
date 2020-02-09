60c0bbe81263f56c64403e22ff604df4d007899d
Pavel <rogozhin.p@yandex.ru>
2020-02-09 09:37:37 +0300

    autowired CategoryDAO with frontend PageController and add list of categorieis

--- online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/controller/PageController.java
+++ online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/controller/PageController.java
@@ -1,42 +1,50 @@
 package com.hadson.onlineshopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.hadson.shoppingbackend.dao.CategoryDAO;
 
 @Controller
 public class PageController {
-	
-    @RequestMapping(value = {"/","/home","/index"})
-    public ModelAndView index(){
 
-	ModelAndView mv = new ModelAndView("page");
-	mv.addObject("title", "Home");
-	mv.addObject("userClickHome", true);
-	return mv;
-	
-    }
+	@Autowired
+	private CategoryDAO categoryDAO;
 
-    @RequestMapping(value = "/about")
-    public ModelAndView about(){
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
 
-	ModelAndView mv = new ModelAndView("page");
-	mv.addObject("title", "About us");
-	mv.addObject("userClickAbout", true);
-	return mv;
-	
-    }
-    
-    @RequestMapping(value = "/contact")
-    public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
 
-	ModelAndView mv = new ModelAndView("page");
-	mv.addObject("title", "Contact us");
-	mv.addObject("userClickContact", true);
-	return mv;
-	
-    }
-	
+		// passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
+
+		mv.addObject("userClickHome", true);
+		return mv;
+
+	}
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact us");
+		mv.addObject("userClickContact", true);
+		return mv;
+
+	}
+
 }
