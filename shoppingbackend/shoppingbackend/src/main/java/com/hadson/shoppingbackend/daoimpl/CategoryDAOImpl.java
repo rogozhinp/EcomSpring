1a2864755c6fb394d52571700b2d81a6e54e42a5
Pavel <rogozhin.p@yandex.ru>
2020-02-09 10:04:32 +0300

    show static categories on homepage

--- shoppingbackend/shoppingbackend/src/main/java/com/hadson/shoppingbackend/daoimpl/CategoryDAOImpl.java
+++ shoppingbackend/shoppingbackend/src/main/java/com/hadson/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -3,9 +3,12 @@ package com.hadson.shoppingbackend.daoimpl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.hadson.shoppingbackend.dao.CategoryDAO;
 import com.hadson.shoppingbackend.dto.Category;
 
+@Repository("categoryDAO")
 public class CategoryDAOImpl implements CategoryDAO {
 
 	private static List<Category> categories = new ArrayList<>();
