a63f74416345959930e65eb24a2ee0a4b33b03ac
Pavel <rogozhin.p@yandex.ru>
2020-02-09 17:01:00 +0300

    annotated Category class and make it to persist in database

--- shoppingbackend/shoppingbackend/src/main/java/com/hadson/shoppingbackend/dao/CategoryDAO.java
+++ shoppingbackend/shoppingbackend/src/main/java/com/hadson/shoppingbackend/dao/CategoryDAO.java
@@ -6,6 +6,8 @@ import com.hadson.shoppingbackend.dto.Category;
 
 public interface CategoryDAO {
 
+	boolean add(Category category);
+
 	List<Category> list();
 
 	Category get(int id);
--- shoppingbackend/shoppingbackend/src/main/java/com/hadson/shoppingbackend/daoimpl/CategoryDAOImpl.java
+++ shoppingbackend/shoppingbackend/src/main/java/com/hadson/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -3,13 +3,19 @@ package com.hadson.shoppingbackend.daoimpl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hadson.shoppingbackend.dao.CategoryDAO;
 import com.hadson.shoppingbackend.dto.Category;
 
 @Repository("categoryDAO")
 public class CategoryDAOImpl implements CategoryDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	private static List<Category> categories = new ArrayList<>();
 
@@ -61,6 +67,24 @@ public class CategoryDAOImpl implements CategoryDAO {
 				return category;
 		}
 		return null;
+
+	}
+
+	@Override
+	@Transactional
+	public boolean add(Category category) {
+
+		try {
+
+			// add the category to the database table
+			sessionFactory.getCurrentSession().persist(category);
+
+			return true;
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
 
 	}
 
