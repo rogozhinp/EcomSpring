229c0d84d4f93a436ca39c48978c1120c74298a2
Pavel <rogozhin.p@yandex.ru>
2020-02-11 18:43:54 +0300

    disable cart button if quantity less 1 for singleProduct and listProduct pages

--- online-shopping/onlineshopping/src/main/webapp/assets/js/myapp.js
+++ online-shopping/onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -71,7 +71,14 @@ $(function() {
 							},
 							{
 
-								data : 'quantity'
+								data : 'quantity',
+								mRender: function(data, type, row){
+									if(data < 1){
+										return '<span style = "color:red">Out of Stock!</span>';
+									}
+									
+									return data;
+								}
 							},
 							{
 
@@ -83,13 +90,27 @@ $(function() {
 											+ '/show/'
 											+ data
 											+ '/product" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a> &#160;';
-									str += '<a href="'
-											+ window.contextRoot
-											+ '/cart/add/'
-											+ data
-											+ '/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+									
+									if(row.quantity < 1){
+										
+										str += '<a href="javascript:void(0)" class="btn btn-success disabled"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+										
+									}
+									else {
+										
+										str += '<a href="'
+												+ window.contextRoot
+												+ '/cart/add/'
+												+ data
+												+ '/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+										
+									}
+									
 									return str;
-								}
+										
+									}
+									
+
 							}
 
 					]
