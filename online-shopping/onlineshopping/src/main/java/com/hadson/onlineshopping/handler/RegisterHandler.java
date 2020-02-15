30d8dad7df93b6b8ed66c43a07d53105c7f4b2b9
Pavel <rogozhin.p@yandex.ru>
2020-02-15 16:39:12 +0300

    create custom validation for email and confirm password fields in registration form

--- online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/handler/RegisterHandler.java
+++ online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/handler/RegisterHandler.java
@@ -1,6 +1,8 @@
 package com.hadson.onlineshopping.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.binding.message.MessageBuilder;
+import org.springframework.binding.message.MessageContext;
 import org.springframework.stereotype.Component;
 
 import com.hadson.onlineshopping.model.RegisterModel;
@@ -54,4 +56,34 @@ public class RegisterHandler {
 
 		return transitionValue;
 	}
+
+	public String validateUser(User user, MessageContext error) {
+
+		String transitionValue = "success";
+
+		// checking if password matches confirm password
+
+		if (!user.getPassword().equals(user.getConfirmPassword())) {
+
+			error.addMessage(new MessageBuilder().error().source("confirmPassword")
+					.defaultText("Password does not match the confirm password").build());
+
+			transitionValue = "failure";
+		}
+
+		// check email for unique of the email id
+
+		if (userDAO.getByEmail(user.getEmail()) != null) {
+
+			error.addMessage(
+					new MessageBuilder().error().source("email").defaultText("Email address is already used").build());
+
+			transitionValue = "failure";
+
+		}
+
+		return transitionValue;
+
+	}
+
 }
