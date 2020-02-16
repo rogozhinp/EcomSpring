1eab5972c31d8ad7a179f90b5f2e2afa5612f56f
Pavel <rogozhin.p@yandex.ru>
2020-02-16 18:51:29 +0300

    set up role based access

--- online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/controller/PageController.java
+++ online-shopping/onlineshopping/src/main/java/com/hadson/onlineshopping/controller/PageController.java
@@ -144,4 +144,15 @@ public class PageController {
 		return mv;
 
 	}
+
+	@RequestMapping(value = "/access-denied")
+	public ModelAndView accessDenied() {
+
+		ModelAndView mv = new ModelAndView("error");
+		mv.addObject("title", "403 - Access Denied");
+		mv.addObject("errorTitle", "Aha! Caught You");
+		mv.addObject("errorDescription", "You are not authorized to view this page!");
+		return mv;
+
+	}
 }
